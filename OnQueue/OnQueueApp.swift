66c2790ea103b72f0c4575a83e1d5b014b8f1f58
Schlicht, Miguel Torres <miguel.schlicht3@gmail.com>
2024-12-05 12:55:28 -0500

    first commit

---
+++ OnQueue/OnQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnQueueApp.swift
+//  OnQueue
+//
+//  Created by Miguel Schlicht on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct OnQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
